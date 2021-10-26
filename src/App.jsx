61b24d357ecfdbbe538dfc1da1a23b0e84366392
techillionaire <techillionaire@gmail.com>
2021-10-26 13:43:45 +0100

    changed App.js to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import Topbar from "./components/topbar/Topbar";
+import Intro from "./components/intro/Intro";
+import About from "./components/about/About";
+import Portfolio from "./components/portfolio/Portfolio";
+import Works from "./components/works/Works";
+import Testimonials from "./components/testimonials/Testimonials";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
 
 const App = () => {
   return (
     <div className="App">
       <Topbar />
+      <div className="sections">
+        <Intro />
+        <About />
+        <Portfolio />
+        <Works />
+        <Testimonials />
+        <Contact />
+        <Footer />
+      </div>
       
     </div>
   )
